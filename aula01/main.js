63771b17b0247969d82903a4b5fda29191a9e842
dodofrb <marcioeduardoassis@gmail.com>
2023-08-24 23:43:08 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,25 @@
+//console.log eu uso par
+//mostrar uma mensagem ao usario
+console.log('hello!');
+
+//alert exibe uma mensagem de alerta
+//ao usario
+
+alert('hello!')
+
+//para declarar variaveis no js
+//podemos usar "var", "let" ou "const"
+
+//SEMPRE que usar "const" para declarar
+//uma variavel,o valor dela NÃO 
+//pode ser alterado!!!
+const pi  = 3.14;
+
+//"let" e uma variavel de escopo local
+//"var" e uma variavel de escopo global
+var nome = "Marcio";
+
+nome = "Maria";
+
+alert(nome);
+
